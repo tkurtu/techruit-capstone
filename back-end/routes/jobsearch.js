93d00ff225aa1527c4bdce01e3dcbc96c1328b24
Tasneem Kurtu <tasneemkurtu@gmail.com>
2019-04-23 12:52:51 -0700

    remove gitignore files

--- back-end/routes/jobsearch.js
+++ back-end/routes/jobsearch.js
@@ -19,18 +19,3 @@ router
 
 
 module.exports = router
-
-
-// router 
-// .route('/alljobs')
-//   .get((req, res, next) => {
-//     axios
-//     .get(jobsAdzuna)
-//     .then(response => {
-//       res.json({
-//         jobs:response.data.results
-//       })
-//     })
-//   })
-
-// const jobsAdzuna = 'https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=f2ae2d10&app_key=e4ae6f67ccd721f8ecab59d03822c56e&results_per_page=100&where=%22Canada%22&content-type=application/json'
